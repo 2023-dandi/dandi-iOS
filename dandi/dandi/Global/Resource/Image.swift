31e062e709155767b79087aed1499527535f0cb9
Yoonseo Kim <yo26on@soongsil.ac.kr>
2023-04-12 19:20:05 +0900

    #51 로그인 화면 이미지 변경

--- dandi/dandi/Global/Resource/Image.swift
+++ dandi/dandi/Global/Resource/Image.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 struct Image {
+    static let logo = UIImage(named: "img.logo")!
+    static let textLogo = UIImage(named: "img.text.logo")!
+    
     static let defaultProfile = UIImage(named: "img.defaultProfile")!
+
     static let btnDelete = UIImage(named: "btnDelete")!
     static let btnRotation = UIImage(named: "btnRotation")!
+
     static let background1 = UIImage(named: "img.background1")!
     static let background2 = UIImage(named: "img.background2")!
     static let background3 = UIImage(named: "img.background3")!
@@ -18,6 +23,7 @@ struct Image {
     static let background5 = UIImage(named: "img.background5")!
     static let background6 = UIImage(named: "img.background6")!
     static let background7 = UIImage(named: "img.background7")!
+
     static let sticker1 = UIImage(named: "img.sticker1")!
     static let sticker2 = UIImage(named: "img.sticker2")!
     static let sticker3 = UIImage(named: "img.sticker3")!
@@ -25,4 +31,22 @@ struct Image {
     static let sticker5 = UIImage(named: "img.sticker5")!
     static let sticker6 = UIImage(named: "img.sticker6")!
     static let sticker7 = UIImage(named: "img.sticker7")!
+    static let largeSticker1 = UIImage(named: "img.big.sticker1")!
+    static let largeSticker4 = UIImage(named: "img.big.sticker4")!
+    static let largeSticker5 = UIImage(named: "img.big.sticker5")!
+
+    static let splash1 = UIImage(named: "img.splash1")!
+    static let splash2 = UIImage(named: "img.splash2")!
+    static let onboarding1 = UIImage(named: "img.onboarding.background1")!
+    static let onboarding2 = UIImage(named: "img.onboarding.background2")!
+    static let onboarding3 = UIImage(named: "img.onboarding.background3")!
+
+    static let cloudLine = UIImage(named: "ic_cloud_line")!
+    static let cloudyLine = UIImage(named: "ic_cloudy_line")!
+    static let raindropsLine = UIImage(named: "ic_raindrops_line")!
+    static let rainyLine = UIImage(named: "ic_rainy_line")!
+    static let snowLine = UIImage(named: "ic_snow_line")!
+    static let snowrainLine = UIImage(named: "ic_snowrain_line")!
+    static let snowyLine = UIImage(named: "ic_snowy_line")
+    static let sunnyLine = UIImage(named: "ic_sunny_line")
 }
